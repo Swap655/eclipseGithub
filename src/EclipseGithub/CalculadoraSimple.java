d8ba2eb43b576fc1a6a8cda790719a6d27366cf2
Fernando <falba20945@joanramis.org>
2024-11-14 19:16:52 +0100

    algo

---
+++ src/EclipseGithub/CalculadoraSimple.java
@@ -0,0 +1,29 @@
+/**@author Fernando*/
+//Agregado desde el branch feature
+package EclipseGithub;
+
+import java.util.Scanner;
+
+public class CalculadoraSimple {
+	public static double sumar(double a, double b) {
+		return a + b;
+	}
+
+	public static double restar(double a, double b) {
+		return a - b;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Calculadora Simple:");
+		System.out.print("Introduce el primer número: ");
+		double numero1 = scanner.nextDouble();
+		System.out.print("Introduce el segundo número: ");
+		double numero2 = scanner.nextDouble();
+		double resultadoSuma = sumar(numero1, numero2);
+		double resultadoResta = restar(numero1, numero2);
+		System.out.println("Resultado de la suma: " + resultadoSuma);
+		System.out.println("Resultado de la resta: " + resultadoResta);
+		scanner.close();
+	}
+}
